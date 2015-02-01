1f86fcba8c443268b4a924812d1aa9e40dbb6b3a
sanuj <sanuj.sharma.in@gmail.com>
2015-02-01 11:18:37 +0530

    Document depth first search

--- ds/graph/depth_first_search.cpp
+++ ds/graph/depth_first_search.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 using namespace std;
 
+// Node of an adjacency list which is used to represent a graph.
 typedef struct struct_node{
 	int data;
 	struct struct_node *next;
 } Node;
 
+// Class representing a Directed Graph
 class Graph {
-	int v;
-	Node **array;
-	void DFSHelper(int src, int *visited);
+	int v; // number of vertices
+	Node **array; // array storing the lists
+	void DFSHelper(int src, int *visited); // recursive helper method for depth first search
 public:
-	Graph(int v);
-	int getV() { return v; };
-	void addEdge(int src, int dest);
-	void DFS(int src);
+	Graph(int v); // constructor for graph
+	int getV() { return v; }; // returns number of vertices
+	void addEdge(int src, int dest); // function to add an edge in the graph
+	void DFS(int src); // depth first search
 	void DFS(); // exhaustive depth first search
-	void printGraph();
+	void printGraph(); // function to print graph in adjacency list form
 };
 
+/* Constructor for Graph
+One parameter:
+v - number of vertices
+*/
 Graph::Graph(int v) {
 	this->v = v;
 	array = new Node*[v];
@@ -26,18 +32,29 @@ Graph::Graph(int v) {
 		array[i] = NULL;
 }
 
+/* Function to add an edge in the Graph
+Two parameters:
+src - source of the edge
+dest - destination of the edge
+*/
 void Graph::addEdge(int src, int dest) {
 	Node *node_src = new Node[1];
 	node_src->data = dest;
 	node_src->next = array[src];
 	array[src] = node_src;
 
+	// Uncomment the following to make the Graph undirectional
 	// Node *node_dest = new Node[1];
 	// node_dest->data = src;
 	// node_dest->next = array[dest];
 	// array[dest] = node_dest;
 }
 
+/* Recursive helper method for DFS
+Two parameters:
+src - source
+visited - array that keeps track of all the visited vertices
+*/
 void Graph::DFSHelper(int src, int *visited) {
 	if(!visited[src]) {
 		cout << src << " ";
@@ -50,6 +67,10 @@ void Graph::DFSHelper(int src, int *visited) {
 	}
 }
 
+/* Depth first search
+One parameter:
+src - source from which DFS should begin
+*/
 void Graph::DFS(int src) {
 	int *visited = new int[v];
 	for(int i=0; i<v; i++)
@@ -57,6 +78,9 @@ void Graph::DFS(int src) {
 	DFSHelper(src, visited);
 }
 
+/* Exhaustive DFS
+Traverses the whole graph in DFS manner
+*/
 void Graph::DFS() {
 	int *visited = new int[v];
 	for(int i=0; i<v; i++)
@@ -68,6 +92,7 @@ void Graph::DFS() {
 	}
 }
 
+/* Prints the graph in adjacency list representation */
 void Graph::printGraph() {
 	for(int i=0; i<v; i++) {
 		Node *node = array[i];
