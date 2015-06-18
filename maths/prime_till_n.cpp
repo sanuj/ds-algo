395ca13eadab5accb860a0792230d25e0c5d9cb9
sanuj <sanuj.sharma.in@gmail.com>
2015-06-18 06:38:23 +0530

    Add algo to find primes till n

---
+++ maths/prime_till_n.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Finds primes till n and saves them in the primes vector
+void getPrimesTillN(int n, vector<int> &primes) {
+	int arr[n];
+
+	// Setting all values to 0
+	for(int i=0; i<n; i++)
+		arr[i] = 0;
+
+	for(int i=1; i<n; i++) {
+		if(arr[i] == 0) {
+			primes.push_back(i+1);
+			int j=2;
+			
+			// Marking all the multiples of (i+1) as 1
+			while(j*(i+1)<=n) {
+				arr[j*(i+1)-1] = 1;
+				j++;
+			}
+		}
+	}
+}
+
+// Function to print the vector of primes
+void printPrimesTillN(int n) {
+	vector<int> primes;
+	getPrimesTillN(n, primes);
+	cout << "List of primes till " << n << ": " << endl;
+	for(int i=0; i<primes.size()-1; i++)
+		cout << primes[i] << ", ";
+	cout << primes[primes.size()-1];
+	cout << endl;
+}
+
+int main() {
+	printPrimesTillN(10);
+	printPrimesTillN(100);
+	return 0;
+}
