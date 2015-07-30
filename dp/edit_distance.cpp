078c66a3638c0f904a0ddee3c95600ac720d86aa
sanuj <sanuj.sharma.in@gmail.com>
2015-07-30 12:21:11 +0530

    Fix Typo

--- dp/edit_distance.cpp
+++ dp/edit_distance.cpp
@@ -13,7 +13,7 @@ int edit_distance(string &a, string &b) {
   cout << endl << "String B: ";
   for(int i=0; i<b.size(); i++)
   	cout << b[i];
-  cout << endl << "Algorithtm converts string B --> A" << endl;
+  cout << endl << "Algorithm converts string B --> A" << endl;
 
   ///// Computing Distance Matrix /////////
   for(int i=0; i<=a.size(); i++)
